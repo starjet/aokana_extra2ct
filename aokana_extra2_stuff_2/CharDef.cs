d65315c4f78e0fb7a0e4ddd45e7b8db8844420bb
Kolitha Kuruppu <starjet@users.noreply.github.com>
2022-12-24 00:58:12 +0530

    Process sprites [WIP, not working quite yet]

---
+++ aokana_extra2_stuff_2/CharDef.cs
@@ -0,0 +1,84 @@
+﻿namespace aokana_extra2_stuff_2
+{
+    internal class CharDef
+    {
+        public CharDef(string _prefix, string _dispname, List<byte> oS, List<byte> oF, int nf1, int nf2, bool alt)
+        {
+            this.prefix = _prefix;
+            this.dispname = _dispname;
+            this.outfitS = oS;
+            this.outfitF = oF;
+            this.nfaceS = (byte)nf1;
+            this.nfaceF = (byte)nf2;
+            this.altpose = alt;
+            this.npose = (byte)(((this.outfitS.Count > 0) ? 1 : 0) + ((this.outfitF.Count > 0) ? 2 : 0) + (this.altpose ? 1 : 0));
+            this.nflypose = (byte)(((this.outfitS.Count > 0) ? 1 : 0) + (this.altpose ? 1 : 0));
+            this.poselist = new char[(int)this.npose];
+            int num = 0;
+            if (this.outfitS.Count > 0)
+            {
+                this.poselist[num++] = 'A';
+            }
+            if (this.altpose)
+            {
+                this.poselist[num++] = 'B';
+            }
+            if (this.outfitF.Count > 0)
+            {
+                this.poselist[num++] = 'F';
+                this.poselist[num++] = 'I';
+            }
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.defoutfit = 0;
+            this.pose = 0;
+            this.face = 0;
+            this.distance = 1;
+            this.active = false;
+            this.slot = -1;
+            if (this.prefix == "mis")
+            {
+                this.defoutfit = 2;
+            }
+            this.outfit = this.defoutfit;
+        }
+
+        public string prefix;
+
+        public string dispname;
+
+        public List<byte> outfitS;
+
+        public List<byte> outfitF;
+
+        public byte nfaceS;
+
+        public byte nfaceF;
+
+        public byte npose;
+
+        public byte nflypose;
+
+        public bool altpose;
+
+        public char[] poselist;
+
+        public bool active;
+
+        public int outfit;
+
+        public int pose;
+
+        public int face;
+
+        public int distance;
+
+        public int slot;
+
+        public int defoutfit;
+    }
+}
+
--- aokana_extra2_stuff_2/Program.cs
+++ aokana_extra2_stuff_2/Program.cs
@@ -1,9 +1,13 @@
 ﻿#pragma warning disable CS8603 // Possible null reference return.
 #pragma warning disable CS8604 // Possible null reference argument.
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 
+using System.Collections.ObjectModel;
 using System.Text;
 using ImageMagick;
+using Newtonsoft.Json;
 
 namespace aokana_extra2_stuff_2
 {
@@ -11,6 +15,21 @@ namespace aokana_extra2_stuff_2
     {
         public static FileStream fs;
         public static Dictionary<string, fe> ti = new Dictionary<string, fe>();
+        public static List<FaceDef> facedef = new List<FaceDef>();
+        public static Dictionary<string, uint> faceidx = new Dictionary<string, uint>();
+        public static List<CharDef> chars;
+
+        public struct FaceDef
+        {
+            public string id;
+            public ushort scalever;
+            public ushort sw;
+            public ushort sh;
+            public ushort x;
+            public ushort y;
+            public ushort w;
+            public ushort h;
+        }
 
         public struct fe
         {
@@ -100,6 +119,226 @@ namespace aokana_extra2_stuff_2
             return cg;
         }
 
+        public static byte[] GetSprite(string fn)
+        {
+            byte[] sprite = Data("sprites/" + fn);
+            sprite ??= Data("hsprites/" + fn);
+            return sprite;
+        }
+
+        public static List<string> TxtToList(byte[] readbuf)
+        {
+            List<string> list = new List<string>();
+            int num = readbuf.Length;
+            int i = 0;
+            while (i < num)
+            {
+                int num2 = i;
+                while (i < num && readbuf[i] != 13 && readbuf[i] != 10)
+                {
+                    i++;
+                }
+                string @string = Encoding.GetEncoding(65001).GetString(readbuf, num2, i - num2);
+                list.Add(@string);
+                if (i < num && readbuf[i] == 13)
+                {
+                    i++;
+                }
+                if (i < num && readbuf[i] == 10)
+                {
+                    i++;
+                }
+            }
+            return list;
+        }
+
+        private static void InitFaceDefData(string fndef, int scalever)
+        {
+            byte[] array = File.ReadAllBytes(fndef);
+            if (array == null)
+            {
+                return;
+            }
+            List<string> list = TxtToList(array);
+            int num = facedef.Count - 1;
+            ushort num2 = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length >= 1)
+                {
+                    if (list[i][0] == '>')
+                    {
+                        string[] array2 = list[i].Split(',', StringSplitOptions.None);
+                        FaceDef item = new FaceDef
+                        {
+                            scalever = (ushort)scalever,
+                            id = array2[0].Substring(1),
+                            sw = Convert.ToUInt16(array2[1]),
+                            sh = Convert.ToUInt16(array2[2]),
+                            x = Convert.ToUInt16(array2[3]),
+                            y = Convert.ToUInt16(array2[4]),
+                            w = Convert.ToUInt16(array2[5]),
+                            h = Convert.ToUInt16(array2[6])
+                        };
+                        facedef.Add(item);
+                        num++;
+                        num2 = 0;
+                    }
+                    else
+                    {
+                        uint value = (uint)(num << 16 | (int)num2);
+                        string key = (scalever > 1) ? ("hi/" + list[i]) : list[i];
+                        faceidx.Add(key, value);
+                        num2 += 1;
+                    }
+                }
+            }
+        }
+
+        private static void GenSeqList(int n, List<byte> L)
+        {
+            byte b = 1;
+            while ((int)b <= n)
+            {
+                L.Add(b);
+                b += 1;
+            }
+        }
+
+        private static void UseSeqList(string[] s, List<byte> L)
+        {
+            foreach (string value in s)
+            {
+                L.Add(Convert.ToByte(value));
+            }
+        }
+
+        private static void LoadCharDef()
+        {
+            List<string> list = TxtToList(File.ReadAllBytes("faceinfo.csv"));
+            chars = new List<CharDef>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Length >= 1 && list[i][0] != ';')
+                {
+                    string[] array = list[i].Split(',', StringSplitOptions.None);
+                    List<byte> list2 = new List<byte>();
+                    List<byte> list3 = new List<byte>();
+                    int nf = 0;
+                    int nf2 = 0;
+                    bool alt = false;
+                    for (int j = 2; j < array.Length; j++)
+                    {
+                        if (array[j][0] == 'B')
+                        {
+                            alt = true;
+                        }
+                        else if (array[j][0] == 'A')
+                        {
+                            int n = Convert.ToInt32(array[j].Substring(1));
+                            GenSeqList(n, list2);
+                        }
+                        else if (array[j][0] == 'F')
+                        {
+                            int n2 = Convert.ToInt32(array[j].Substring(1));
+                            GenSeqList(n2, list3);
+                        }
+                        else if (array[j][0] == 'a')
+                        {
+                            string[] s = array[j].Substring(1).Split('/', StringSplitOptions.None);
+                            UseSeqList(s, list2);
+                        }
+                        else if (array[j][0] == 'f')
+                        {
+                            string[] s2 = array[j].Substring(1).Split('/', StringSplitOptions.None);
+                            UseSeqList(s2, list3);
+                        }
+                        else if (array[j][0] == 'H')
+                        {
+                            nf = Convert.ToInt32(array[j].Substring(1));
+                        }
+                        else if (array[j][0] == 'G')
+                        {
+                            nf2 = Convert.ToInt32(array[j].Substring(1));
+                        }
+                    }
+                    CharDef item = new CharDef(array[0], array[1], list2, list3, nf, nf2, alt);
+                    chars.Add(item);
+                }
+            }
+        }
+
+        private static string UpdateSprite(CharDef charDef)
+        {
+            char c = charDef.poselist[charDef.pose];
+            char c2;
+            List<byte> list;
+            if (c <= 'B')
+            {
+                if (c == 'A' || c == 'B')
+                {
+                    c2 = 'H';
+                    list = charDef.outfitS;
+                    goto IL_65;
+                }
+            }
+            else
+            {
+                if (c == 'F')
+                {
+                    c2 = 'G';
+                    list = charDef.outfitF;
+                    goto IL_65;
+                }
+                if (c == 'I')
+                {
+                    c2 = 'J';
+                    list = charDef.outfitF;
+                    goto IL_65;
+                }
+            }
+            c2 = ' ';
+
+            list = null;
+        IL_65:
+            string text = "";
+            int num = charDef.face + 1;
+            string fname = "";
+            //Console.WriteLine("test" + JsonConvert.SerializeObject(charDef));
+            if (charDef.prefix != "sit")
+            {
+                string text2 = "a";
+                switch (charDef.distance)
+                {
+                    case 0:
+                        text = "SS";
+                        break;
+                    case 1:
+                        text = "S";
+                        break;
+                    case 2:
+                        text = "M";
+                        break;
+                    case 3:
+                        text = "L";
+                        break;
+                }
+                fname = string.Format("{0}{1}{2:00}{3}+{4}{5}{6:00}{7}{8}", new object[]
+                {
+                charDef.prefix,
+                c,
+                list[charDef.outfit],
+                text,
+                charDef.prefix,
+                c2,
+                num,
+                text,
+                text2
+                });
+            }
+            return fname;
+        }
+
         static void Main(string[] args)
         {
             string path = args[0];
@@ -132,6 +371,7 @@ namespace aokana_extra2_stuff_2
             }
             if (path.Contains("evcg.dat") || path.Contains("adult.dat"))
             {
+                Directory.CreateDirectory("_out_cgs");
                 string[] cgList = File.ReadAllLines("vcglist.csv");
                 foreach (string cg in cgList)
                 {
@@ -139,7 +379,6 @@ namespace aokana_extra2_stuff_2
                     string cgName = cgVariants[0];
                     Console.WriteLine(cgName);
                     byte[] baseImage = GetCg(cgVariants[1] + ".webp");
-                    Directory.CreateDirectory("_out");
                     if (baseImage != null)
                     {
                         MagickImage magickImage = new MagickImage(baseImage);
@@ -148,7 +387,74 @@ namespace aokana_extra2_stuff_2
                             MagickImage variantPart = new MagickImage(GetCg(cgVariants[i] + ".webp"));
                             magickImage.Composite(variantPart, CompositeOperator.Over);
                         }
-                        magickImage.Write(@"_out\" + cgName + ".jpg");
+                        magickImage.Write(@"_out_cgs\" + cgName + ".jpg");
+                    }
+                }
+            }
+            if (path.Contains("sprites.dat") || path.Contains("adult.dat"))
+            {
+                Directory.CreateDirectory("_out_sprites");
+                InitFaceDefData("defs.txt", 1);
+                LoadCharDef();
+                List<string> sprites = new List<string>();
+                foreach (CharDef char1 in chars)
+                {
+                    foreach (char pose in char1.poselist)
+                    {
+                        for (int dist = 0; dist < 4; dist++)
+                        {
+                            IEnumerable<byte> outfits = char1.outfitS.Union(char1.outfitF);
+                            foreach (byte outfit in outfits)
+                            {
+                                char1.pose = Array.IndexOf(char1.poselist, pose);
+                                char1.distance = dist;
+                                List<byte> outfitList = pose <= 'B' ? char1.outfitS : char1.outfitF;
+                                char1.outfit = outfitList.IndexOf(outfit);
+                                if (char1.outfit > -1)
+                                {
+                                    string sprite = UpdateSprite(char1);
+                                    if (sprite != "")
+                                    {
+                                        sprites.Add(sprite);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach (string sprite in sprites)
+                {
+                    string[] spriteSplit = sprite.Split('+');
+                    string spriteBase = spriteSplit[0];
+                    string spriteFace = spriteSplit[1];
+                    byte[] baseData = GetSprite(spriteBase.ToLower() + ".webp");
+                    if (baseData != null)
+                    {
+                        uint value;
+                        if (faceidx.TryGetValue(spriteFace, out value))
+                        {
+                            int index = (int)(value >> 16);
+                            int pos = (int)(value & 65535U);
+                            FaceDef faceDef = facedef[index];
+                            byte[] faceData = GetSprite(faceDef.id.ToLower() + ".webp");
+                            if (faceData != null)
+                            {
+                                MagickImage faceImage = new MagickImage(faceData);
+                                //faceImage.Crop(faceDef.w, faceDef.h);
+                                //baseImage.Composite(faceImage, faceDef.x, faceDef.y, CompositeOperator.Over);
+                                //baseImage.Write(@"_out_sprites\" + sprite.Replace('+', '_') + ".png");
+                                //IReadOnlyCollection<IMagickImage<byte>> faceParts = faceImage.CropToTiles(faceDef.w, faceDef.h);
+                                //int i = 0;
+                                //foreach (IMagickImage<byte> face in faceParts)
+                                //{
+                                //    Console.WriteLine(sprite.Replace('+', '_') + (++i).ToString());
+                                //    //MagickImage baseImage = new MagickImage(baseData);
+                                //    //baseImage.Composite(face, faceDef.x, faceDef.y, CompositeOperator.Over);
+                                //    //baseImage.Write(@"_out_sprites\" + sprite.Replace('+', '_') + (++i).ToString() + ".png");
+                                //}
+                                Console.WriteLine(sprites.ToArray().Length);
+                            }
+                        }
                     }
                 }
             }
